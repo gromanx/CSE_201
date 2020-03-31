97cc9f80ad8d733ae27014f071c0ecdc73240284
gromanx <54556129+gromanx@users.noreply.github.com>
2020-03-31 16:05:36 -0700

    Add files via upload

--- LAB1/Hello.cpp
+++ LAB1/Hello.cpp
@@ -1,1 +1,16 @@
+/*
+ * Gladys Roman
+ * Hello.cpp
+ * This program will display a Hello message in C++
+ * January 9, 2019
+ */
 
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello, World!\nWelcome to CSE201!\nMy name is Gladys Roman.\n";
+
+	return 0;
+}
